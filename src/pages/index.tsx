c18da92346e0150e2c34973d50f590c48d220e04
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-23 10:51:11 -0300

    changing project structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from "../styles/components/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+
+      <section>
+        <div style={{ border: "1px #ff0000 solid" }}>
+          <Profile />
+        </div>
+        <div style={{ border: "1px #0000ff solid" }}></div>
+      </section>
     </div>
   );
 }
