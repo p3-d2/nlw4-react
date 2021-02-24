d11c680595c55c1c6dd90516dafedb2d671b347d
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-24 13:51:50 -0300

    creating and styling ChallengeBox component

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,38 @@
+import styles from "../styles/components/ChallengeBox.module.css";
+
+export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg" alt="body" />
+            <strong>Novo desafio</strong>
+            <p>Levnte e faça uma caminhada de 3 minutis</p>
+          </main>
+
+          <footer>
+            <button type="button" className={styles.challengeFailButton}>
+              Falhei
+            </button>
+            <button type="button" className={styles.challengeSuceededButton}>
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
