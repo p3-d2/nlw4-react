3916e63b92253777963fe5c87de83dd0f877c796
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-24 14:47:09 -0300

    creating hook to access the context

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,21 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState, useContext } from "react";
+import challenges from "../../challenges.json";
+
+interface Challenge {
+  type: "body" | "eye";
+  description: string;
+  amount: number;
+}
 
 interface ValueProps {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
+  experienceToNextLevel: number;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -16,15 +26,26 @@ export const ChallengesContext = createContext({} as ValueProps);
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
-  const [currentExperience, setCurrentExperience] = useState(0);
+  const [currentExperience, setCurrentExperience] = useState(13);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  const [activeChallenge, setActiveChallenge] = useState({} as Challenge);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log("challenge");
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge as Challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge({} as Challenge);
   }
 
   return (
@@ -35,9 +56,36 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         currentExperience,
         challengesCompleted,
         startNewChallenge,
+        activeChallenge,
+        resetChallenge,
+        experienceToNextLevel,
       }}
     >
       {children}
     </ChallengesContext.Provider>
   );
+}
+
+export function useChallengeContext() {
+  const {
+    level,
+    levelUp,
+    currentExperience,
+    challengesCompleted,
+    startNewChallenge,
+    activeChallenge,
+    resetChallenge,
+    experienceToNextLevel,
+  } = useContext(ChallengesContext);
+
+  return {
+    level,
+    levelUp,
+    currentExperience,
+    challengesCompleted,
+    startNewChallenge,
+    activeChallenge,
+    resetChallenge,
+    experienceToNextLevel,
+  };
 }
